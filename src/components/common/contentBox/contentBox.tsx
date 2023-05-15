e60ce96ce038f4024734bfa76c51c68fd6918501
Ellie <doris0707@naver.com>
2023-05-15 17:20:17 +0900

    feat: 업로드 페이지 설계

---
+++ src/components/common/contentBox/contentBox.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function ContentBox(){
+    return(
+        <Box>
+            is ContentBox
+        </Box>
+    );
+}
---
+++ src/components/common/refreshButton/refreshButton.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function RefreshButton(){
+    return(
+        <Box>
+            is refreshButton
+        </Box>
+    );
+}
