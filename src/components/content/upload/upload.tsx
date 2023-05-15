0325f1e88bdb1835f1fe7d362dd740b46c0c4412
Ellie <doris0707@naver.com>
2023-05-15 17:23:46 +0900

    feat: 업로드 페이지 컨턴츠, 컴포넌트  설계/ common 파일 구조 변경

---
+++ src/components/content/upload/upload.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function Upload(){
+    return(
+        <Box>
+            is upload content
+        </Box>
+    );
+}
