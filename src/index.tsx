a14c3878e37f2c4991bf226482d097dcca9a3d09
Eunseo Na <nes020781@gmail.com>
2023-05-18 22:56:11 +0900

    feat: index.tsx에서 redux 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ProSidebarProvider } from 'react-pro-sidebar';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import store from './stores/store';
+
+export let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <ProSidebarProvider>
-    <App />
-    </ProSidebarProvider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+    <React.StrictMode>
+      <ProSidebarProvider>
+      <App />
+      </ProSidebarProvider>
+    </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
