069f38e065ecad62ad5e98a7b371b985e51bd0ec
Eunseo Na <nes020781@gmail.com>
2023-08-15 23:11:50 +0900

    feat: 브랜치 이름 수정, 프로젝트 삭제 모달 퍼블리싱

--- src/components/common/box/branchBox/branchBox.tsx
+++ src/components/common/box/branchBox/branchBox.tsx
@@ -1,9 +1,37 @@
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Modal, Typography, Button, Input } from "@mui/material";
+import { useEffect, useState } from "react";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import CheckIcon from '@mui/icons-material/Check';
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 450,
+  height: 200,
+  bgcolor: 'background.paper',
+  border: 'none',
+  borderRadius: '3px',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function BranchBox() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const ariaLabel = { 'aria-label': 'description' };
+
+  const [branchName, setBranchName] = useState('');
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+  
   return(
     <Box display="flex" sx={{ width:"930px", alignItems: "center"}}>
       <Box sx={{width:"250px",fontSize: "20px", fontWeight: "bold", textAlign: "left", textOverflow: 'ellipsis'}}>
@@ -13,9 +41,44 @@ export default function BranchBox() {
         last update : 2022/08/14 19:00
       </Box>
       <Box sx={{display: "flex", alignItems: "center", ml:"auto"}}>
-        <IconButton>
+        <IconButton
+          onClick={() => handleModalOpen()}
+        >
           <EditIcon/>
         </IconButton>
+
+        <Modal
+          open={isModalOpen}
+          onClose={handleModalClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+        >
+          <Box sx={style}>
+            <Box sx = {{
+            display: "flex",
+            flexDirection: 'column',
+            justifyItems: 'center',
+            alignItems: 'center',
+            }}>
+              <Typography id="modal-modal-title" variant="h4" component="h2">
+                Edit Branch
+              </Typography>
+              
+              <Input 
+                placeholder="name" 
+                inputProps={ariaLabel}
+                value={branchName}
+                onChange={(event)=>(setBranchName(event.target.value))}
+                sx={{width:'300px', marginBottom: '30px', marginTop: '50px'}}
+              />
+              
+
+              <Button variant="outlined" startIcon={<CheckIcon />}>
+                Edit
+              </Button>
+            </Box>
+          </Box>
+        </Modal>
 
         <IconButton>
           <DeleteIcon/>
--- src/components/common/box/deleteBox/deleteBox.tsx
+++ src/components/common/box/deleteBox/deleteBox.tsx
@@ -1,15 +1,83 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Modal, Typography, Input } from "@mui/material";
+import { useState, useEffect } from "react";
+import CheckIcon from '@mui/icons-material/Check';
+import axios from "axios";
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 450,
+  height: 220,
+  bgcolor: 'background.paper',
+  border: 'none',
+  borderRadius: '3px',
+  boxShadow: 24,
+  p: 4,
+};
+
 
 export default function DeleteBox() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const ariaLabel = { 'aria-label': 'description' };
+
+  const [branchName, setBranchName] = useState('');
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+  
   return(
     <Box display="flex" sx={{ width:"930px", alignItems: "center"}}>
       <Box sx={{width:"250px",fontSize: "22px", fontWeight: "bold", textAlign: "left", textOverflow: 'ellipsis', color: "#FF3A3A"}}>
         Delete Project
       </Box>
       <Box sx={{display: "flex", alignItems: "center", ml:"auto"}}>
-        <Button sx={{width: "150px", height: "50px", color: "white", bgcolor: "#FF3A3A"}}>
+        <Button sx={{width: "150px", height: "50px", color: "white", bgcolor: "#FF3A3A"}}
+          onClick={() => handleModalOpen()}
+        >
           delete
         </Button>
+        <Modal
+          open={isModalOpen}
+          onClose={handleModalClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+        >
+          <Box sx={style}>
+            <Box sx = {{
+            display: "flex",
+            flexDirection: 'column',
+            justifyItems: 'center',
+            alignItems: 'center',
+            }}>
+              <Typography id="modal-modal-title" variant="h4" component="h2">
+                Delete Project
+              </Typography>
+              <Box sx={{mt: "5px",fontSize: "16px", fontWeight: "regular", textAlign:"center"}}>
+                삭제할 프로젝트 이름을 입력해 주세요.
+              </Box>
+              
+              <Input 
+                placeholder="프로젝트 이름" 
+                inputProps={ariaLabel}
+                value={branchName}
+                onChange={(event)=>(setBranchName(event.target.value))}
+                sx={{width:'300px', marginBottom: '30px', marginTop: '50px'}}
+              />
+              
+
+              <Button sx={{bgcolor: "#FF3A3A", color: "white", width: "125px"}} startIcon={<CheckIcon />}>
+                Delete
+              </Button>
+            </Box>
+          </Box>
+        </Modal>
       </Box>
     </Box>
   );
